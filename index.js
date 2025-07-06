d08cdfe7713d83fe06bbb8aa56d488ef4740a8ec
bibhu-git <bibhuranjanm324@gmail.com>
2025-07-07 01:38:52 +0530

    modify CORS

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT ||  4000;
 //middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'https://fooddeliveryfrontend-gamma.vercel.app',
+  origin: 'https://food-delivery-frontend-two-gamma.vercel.app',
   credentials: true,
 }));
 app.use("/api/food",foodRouter);
